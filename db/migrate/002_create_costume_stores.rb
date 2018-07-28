94aebb9ceba957c6d62b74eb283f34b966b0eec5
Catherine Bronzo <cbronzo@mac.com>
2018-07-28 02:39:59 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,6 +9,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :employee_count
       t.boolean :still_in_business
+      t.datetime :opening_date
+      t.datetime :closing_date
+      
       
     end
   end
